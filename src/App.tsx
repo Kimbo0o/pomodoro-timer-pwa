d9b8b8d8f93eba296b53f8d299a1cbe3fd0751fa
Kimbo0o <kimkoch1000@gmail.com>
2021-10-05 07:42:53 +0200

    Added basic setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Frame from './Components/UI/Frame';
+
+function App() {
+  return (
+    <div className="App">      
+    <Frame>test</Frame>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UI/Frame.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Frame.module.scss";
+
+const Frame: React.FC = (props) =>{
+    return <section className={classes.frame}>        
+            {props.children}             
+        </section>
+}
+
+export default Frame;
