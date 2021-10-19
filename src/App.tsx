a31fb439abc4eb566788986ba7fb533a00c62ac4
Kimbo0o <kimkoch1000@gmail.com>
2021-10-19 07:38:54 +0200

    Added Basic UI Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Bubble from './Components/UI/Bubble';
 import Frame from './Components/UI/Frame';
 
 function App() {
   return (
     <div className="App">      
     <Frame>test</Frame>
+    <Bubble top="-20px" left="-50px" degree={135} ></Bubble>
+    <Bubble top="400px" right="-40px" degree={315} ></Bubble>
+    <Bubble top="700px" left="-20px" degree={315} ></Bubble>
     </div>
   );
 }
---
+++ src/Components/Timer/TimerSelectBtns.tsx
@@ -0,0 +1,7 @@
+import classes from "./TimerSelectBtns.module.scss";
+
+const TimerSelectBtns:React.FC = (props) => {
+    return<div></div>
+}
+
+export default TimerSelectBtns;
