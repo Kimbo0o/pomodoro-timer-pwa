250e99a46d9a2a2b2bec07185818b714720d51c2
Kimbo0o <kimkoch1000@gmail.com>
2021-10-20 07:08:20 +0200

    Updated App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import './App.css';
 import Bubble from './Components/UI/Bubble';
 import Frame from './Components/UI/Frame';
+import classes from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">      
-    <Frame>test</Frame>
-    <Bubble top="-20px" left="-50px" degree={135} ></Bubble>
-    <Bubble top="400px" right="-40px" degree={315} ></Bubble>
-    <Bubble top="700px" left="-20px" degree={315} ></Bubble>
+    <div className={classes.App}>      
+      <div className={classes["main-container"]}>
+        <Frame>test</Frame>
+        <Bubble top="-20px" left="-50px" degree={135} ></Bubble>
+        <Bubble top="400px" right="-40px" degree={315} ></Bubble>
+        <Bubble top="700px" left="-20px" degree={315} ></Bubble>
+      </div>    
     </div>
   );
 }
